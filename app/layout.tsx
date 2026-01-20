c17f77229009540b7e54cbc57fbd813c7cd844ed
car9313 <claudiaar9313@gmail.com>
2026-01-19 16:17:39 -0800

    Creacion de la vista de categorias

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import { ThemeProvider } from "../components/theme-provider";
 import AnimatedBackground from "../components/animated-background";
 import Navbar from "../components/Navbar";
+import Footer from "@/components/Footer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -65,6 +66,7 @@ export default function RootLayout({
                 {children}
               </div>
             </div>
+              <Footer />
           </div>
         </ThemeProvider>
 
--- components/CategoryTabs.tsx
+++ components/CategoryTabs.tsx
@@ -28,20 +28,31 @@ export function CategoryTabs({
             animate={{ opacity: 1, y: 0 }}
             transition={{ duration: 0.6, delay: 0.3 }}
 
+
+/* 
+<Badge
+                                key={tag}
+                                variant="outline"
+                                className="text-xs bg-linear-to-r from-blue-50 to-purple-50 dark:from-blue-900/50 dark:to-purple-900/50 border-blue-200 dark:border-blue-700 text-blue-700 dark:text-blue-300 hover:scale-105 transition-transform duration-200"
+                                style={{ animationDelay: `${index * 50}ms` }}
+                            >
+                                {tag}
+                            </Badge>
+*/
+
             className="w-full max-w-4xl mx-auto mb-12">
-          <button onClick={()=>alert('Hola Mundo')}>Hola Mundo</button>
             <div className="flex flex-wrap justify-center gap-2 p-1 bg-white/50 dark:bg-slate-800/50 backdrop-blur-sm rounded-full border-b border-border/40 shadow-lg">
                 {categories.map((category) => (
                     <Link
                         key={category}
-                        href={`/?${createQueryString("category", category)}`}
+                        href={`categorias/?${createQueryString("category", category)}`}
                         onClick={() => {
                             setActiveCategory(category)
                         }}
                         className={
-                            `px-4 py-2 rounded-full transition-all duration-500 ${activeCategory === category
-                                ? "bg-primary text-white shadow-lg"
-                                : "bg-white dark:bg-gray-800 text-gray-700 dark:text-gray-300 hover:bg-gray-100 dark:hover:bg-gray-700"
+                            `px-4 py-2 hover:scale-90 rounded-full transition-all duration-500 ${activeCategory === category
+                                ? "bg-secondary  bg-linear-to-r from-purple-100 to-pink-100 dark:from-purple-900/50 dark:to-pink-900/50 text-purple-700 dark:text-purple-300 border-purple-200 dark:border-purple-700"
+                                : "bg-linear-to-r from-blue-50 to-purple-50 dark:from-blue-900/50 dark:to-purple-900/50 border-blue-200 dark:border-blue-700 text-blue-700 dark:text-blue-300"
                             }`}
                     >
                         {category}
